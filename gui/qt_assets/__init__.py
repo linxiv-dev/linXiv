e144a5f92cf4bca814e559377d9c2a236597cc4d
jakeuribe <jake.uribe@gmail.com>
2026-05-18 12:13:34 -0500

    fix in init

--- gui/qt_assets/__init__.py
+++ gui/qt_assets/__init__.py
@@ -2,6 +2,8 @@ from .paper_card import PaperCard, ElidedLabel
 from .selection_bar import SelectionBar
 from .add_paper_dialog import AddPaperManuallyDialog
 from .export_import_buttons import ProjectExportButton, ProjectImportButton
+from .trash_panel import TrashPanel
+from .workers import PdfMetadataWorker
 
 __all__ = [
     "PaperCard",
@@ -10,6 +12,6 @@ __all__ = [
     "AddPaperManuallyDialog",
     "ProjectExportButton",
     "ProjectImportButton",
-    "TrashPanel", 
-    "PdfMetadataWorker"
+    "TrashPanel",
+    "PdfMetadataWorker",
 ]
