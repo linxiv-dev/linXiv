5a59ff7e40d76b3196c77bcbbd5a7320b7f8108c
jakeuribe <jake.uribe@gmail.com>
2026-05-17 19:15:31 -0500

    path changes

--- storage/migrations/projects_rebuild_swap.sql
+++
@@ -1,4 +0,0 @@
--- Swap projects_intermediate into place after the dynamic INSERT SELECT has run.
-DROP TABLE projects;
-ALTER TABLE projects_intermediate RENAME TO projects;
-CREATE INDEX IF NOT EXISTS idx_projects_status ON projects(status);
